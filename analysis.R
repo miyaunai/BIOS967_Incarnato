581e864b2be0473081b01ca64e967f29562118c7
miyaunai <142921780+miyaunai@users.noreply.github.com>
2023-12-07 06:40:13 -0600

    WOOO
    WE DID IT

--- analysis.R
+++ analysis.R
@@ -34,10 +34,19 @@ parents_area=parents%>%group_by(file) %>%
   mutate(parents_id=str_replace(file, "M_PC_23_G_", "")) %>%
   select(-file)
 
-offspring_mean %>% left_join(., parents_area, by=join_by(parent1==parents_id)) %>%
-left_join(., parents_area, by=join_by(parent2==parents_id))
+area_data=offspring_mean %>% left_join(., parents_area, by=join_by(parent1==parents_id)) %>%
+left_join(., parents_area, by=join_by(parent2==parents_id)) %>% 
+  mutate(midparent_area=(area.y+area.x)/2)
 
 
 #parents_id=pull(parents_mean, file) %>% str_replace("M_PC_23_G_", "")
 
+po_reg_area<-glm(area_data$offspring_area~area_data$midparent_area)
+
+ggplot(data=area_data,aes(x=midparent_area, y=offspring_area))+
+  geom_point()+
+  geom_smooth(method=glm)
+
+po_reg_area<-glm(area_data$offspring_area~area_data$midparent_area)
+ anova(po_reg_area)
 
